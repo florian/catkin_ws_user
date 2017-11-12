cd97842d5c5e2dfbb67f0471bccc4aa93e3d3a8c
Florian Hartmann <jscoderr@gmail.com>
2017-11-12 17:09:06 +0100

    Add our changes for the third assignment

--- src/py_image_processing/image_processing_example.py
+++ src/py_image_processing/image_processing_example.py
@@ -12,6 +12,9 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 #import matplotlib
 #matplotlib.use('Agg')
+
+import numpy as np
+
 from matplotlib import pyplot as plt
 
 # from __future__ import print_function
@@ -20,6 +23,10 @@ class image_converter:
 
   def __init__(self):
     self.image_pub = rospy.Publisher("/image_processing/bin_img",Image, queue_size=1)
+    self.gray_pub = rospy.Publisher("/image_processing/gray_img",Image, queue_size=1)
+    self.threshold_pub = rospy.Publisher("/image_processing/threshold_img",Image, queue_size=1)
+
+
 
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/app/camera/rgb/image_raw",Image,self.callback, queue_size=1)
@@ -34,11 +41,13 @@ class image_converter:
 
     #make it gray
     gray=cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+    self.gray_pub.publish(self.bridge.cv2_to_imgmsg(gray,"mono8"))
 
     #bi_gray
-    bi_gray_max = 255
-    bi_gray_min = 245
+    bi_gray_max = 245
+    bi_gray_min = 215
     ret,thresh1=cv2.threshold(gray, bi_gray_min, bi_gray_max, cv2.THRESH_BINARY);
+    self.threshold_pub.publish(self.bridge.cv2_to_imgmsg(thresh1,"mono8"))
 
     #gauss
     MAX_KERNEL_LENGTH = 2;
@@ -60,21 +69,63 @@ class image_converter:
     g_min = 0;
     b_max = 255;
     b_min = 0;
-    b,g,r = cv2.split(cv_image)
-    for j in range(cv_image.shape[0]):
-      for i in range(cv_image.shape[1]):
-        if (r[j,i] >= r_min and r[j,i] <= r_max):
-          if (g[j,i] >= g_min and g[j,i] <= g_max):
-            if (b[j,i] >= b_min and b[j,i] <= b_max):
-              r[j,i]=0
-              g[j,i]=0
-              b[j,i]=0
-            else:
-              r[j,i]=255
-              g[j,i]=255
-              b[j,i]=255
-    bi_rgb = cv2.merge((b,g,r))
 
+    thresh_flatten = thresh1.flatten()
+    bool_lst = (thresh_flatten>=245).reshape(thresh1.shape)
+    white_points =  zip(*np.where(bool_lst))
+
+    objectPoints = np.array([
+    [28, 0, 0],
+    [28, 23, 0],
+    [60, 0, 0],
+    [60, 23, 0],
+    [90, 0, 0],
+    [90, 23, 0] ], dtype = np.float).reshape((6,3,1))
+
+    imagePoints = np.array([
+    [160, 420],
+    [160, 310],
+    [225, 450],
+    [220, 300],
+    [340, 500],
+    [320, 290] ], dtype = np.float).reshape((6, 2, 1))
+
+    cameraMatrix = np.array([
+    [614.1699, 0, 329.9491],
+    [0, 614.9002, 237.2788],
+    [0, 0, 1] ])
+
+    distCoeffs = np.array([[0.1115, -0.1089, 0, 0]])
+
+    rotation_vector = np.zeros((3,1))
+    translation_vector = np.zeros((3,1))
+
+
+    _, rotation_vector, translation_vector = cv2.solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeffs)
+
+
+    rotation_matrix, _ = cv2.Rodrigues(rotation_vector)
+
+    inv_rotation_matrix = rotation_matrix.T
+    inv_rotation_vector = np.dot(-inv_rotation_matrix,rotation_vector)
+
+    yaw = np.arctan2(inv_rotation_matrix[1,0], inv_rotation_matrix[0,0])
+    pitch = np.arctan2(-inv_rotation_matrix[2,0], np.sqrt(np.power(inv_rotation_matrix[2,1],2) + np.power(inv_rotation_matrix[2,2],2)))
+    roll = np.arctan2(inv_rotation_matrix[2,1],inv_rotation_matrix[2,2] )
+
+
+    print "yaw", yaw
+    print "pitch", pitch
+    print "roll",roll
+
+
+    projected_points, _ = cv2.projectPoints(objectPoints, rotation_vector, translation_vector, cameraMatrix, distCoeffs)    
+    print projected_points
+
+
+
+
+    #bi_rgb = cv2.merge((b,g,r))
     #bi_hsv
     h_max = 255;
     h_min = 0;
